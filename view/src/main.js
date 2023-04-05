f6822b4d0138d1624456fafd058a626f9465a59d
Barak Binaymin <barak.binyamin555@gmail.com>
2023-04-05 01:31:24 -0400

    Updated fues search options

--- view/src/main.js
+++ view/src/main.js
@@ -10,10 +10,11 @@ app.use(router)
 import Fuse from 'fuse.js'
 import database from './assets/database.json'
 const options = {
+    ignoreLocation: true,
     includeScore  : true,
     includeMatches: true,
     // Search in `author` and in `tags` array
-    keys: ['title','content','tags','id']
+    keys: ['title','description','content','tags','id']
 }
 const fuse = new Fuse(database, options)
 app.config.globalProperties.$fuse = ref(fuse) // usage this.$fuse.value.search("whatever")
