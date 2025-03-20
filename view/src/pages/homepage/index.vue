e3f1859a2548302e43abfc1a5269e414168fd483
Barak Binaymin <barak.binyamin555@gmail.com>
2025-03-20 01:42:02 -0400

    Update link

--- view/src/pages/homepage/index.vue
+++ view/src/pages/homepage/index.vue
@@ -40,7 +40,7 @@
         <div class="welcome">Hey there 👋, I'm Barak, just another dude in Boston. When I'm not at work, you can usually find me hanging with friends, rock climbing, dancing west coast swing, or stiching together side projects in a coffee shop.
           Thanks for checking out my website, I enjoy meeting new people so don't hesitate to <a class="link" target="_blank" href="https://ig.me/m/barak_binyamin">reach out on instagram</a>!  
           <br><br>Follow my <a class="link" href="/blog">blog</a> posts for high quality banter, check out <a class="link" href="/table">the table</a> for misc table talk ideas, and don't forget to stop by the <a class="link" href="/store">store</a>
-          <br><br>Also reach out if you could use a free portfolio, or you'd like to learn more about how to code. I tutor CS/CE/SWEN and I'm developing a <a class="link" target="_blank" href="">course</a> that could use some feedback!
+          <br><br>Also reach out if you could use a free portfolio, or you'd like to learn more about how to code. I tutor CS/CE/SWEN and I'm developing a <a class="link" target="_blank" href="https://github.com/BarakBinyamin/amaze">course</a> that could use some feedback!
           <!-- <br><br>Here are some links to rojects I'm working on, feel free to reach out if you want to contribute! -->
 
         </div>
